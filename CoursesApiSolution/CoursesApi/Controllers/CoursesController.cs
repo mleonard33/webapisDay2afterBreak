ba77f427a257e6a297b5c2811b1fdb12534972cb
mleonard33 <55216946+mleonard33@users.noreply.github.com>
2022-08-23 15:34:20 -0400

    Initial commit

---
+++ CoursesApiSolution/CoursesApi/Controllers/CoursesController.cs
@@ -0,0 +1,45 @@
+﻿using CoursesApi.Domain;
+
+namespace CoursesApi.Controllers;
+
+public class CoursesController : ControllerBase
+{
+
+    private readonly CourseManager _courseManager;
+   
+
+    public CoursesController(CourseManager courseManager)
+    {
+        _courseManager = courseManager;
+    }
+
+    [HttpGet("/courses")]
+    public async Task<ActionResult<CoursesResponse>> GetAllCoursesAsync()
+    {
+        // WTCYWYH
+
+        CoursesResponse response = await _courseManager.GetAllCoursesAsync();
+
+        return Ok(response);
+        
+    }
+
+    [HttpGet("/courses/{courseId}")]
+    public async Task<ActionResult<CourseDetailsResponse>> GetCourseByIdAsync(string courseId)
+    {
+        if(courseId == "Dance")
+        {
+            return NotFound();
+        } else
+        {
+            var response = new CourseDetailsResponse
+            {
+                Id = courseId,
+                Title = "Underwater basket Weaving",
+                DeliveryLocation = DeliveryLocationTypes.Online,
+                NumberOfHours = 323.5M
+            };
+            return Ok(response);
+        }
+    }
+}
---
+++ CoursesApiSolution/CoursesApi/Domain/CourseManager.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+
+namespace CoursesApi.Domain;
+
+public class CourseManager
+{
+
+    private readonly MongoDbCoursesAdapter _adapter;
+
+    public CourseManager(MongoDbCoursesAdapter adapter)
+    {
+        _adapter = adapter;
+    }
+
+    public async Task<CoursesResponse> GetAllCoursesAsync()
+    {
+        var coursesFromDatabase = await _adapter.Courses.Find(t => true).ToListAsync();
+
+        var response = new CoursesResponse
+        {
+            Data = coursesFromDatabase.Select(c => new CourseSummaryItemResponse
+            {
+                Id = c.Id.ToString(),
+                Title = c.Title
+               
+            }).ToList(),
+        };
+
+        return response;
+    }
+}
